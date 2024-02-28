66a3ed3f0b68f8c56a01aa7f933527137a818019
Molkang <mitier26@gmail.com>
2024-02-28 23:40:07 +0900

    set books

--- javascript/script.js
+++ javascript/script.js
@@ -1,61 +1,92 @@
 // 문서가 전부 로딩되면 작동하라는 뜻
-window.onload = function () {
-    // 스크립트 작성 하는 곳
-    const API_KEY = `f451f1b377d0055a79e2ed0208d5a45c4f1ec7a8edce14b2e85f5f09d19ef2db`;
-    let url = new URL('https://www.nl.go.kr/NL/search/openApi/search.do?');
-    let bookList = [];
+// window.onload = function () {
+// 스크립트 작성 하는 곳
+/*
+        작성자 : 우명균
+    */
+// API 키를 배열로 만들어 키가 막혔을 때 다른 키를 사용한다.
+// ttbmitier1409002
+const API_KEY = [`09f34dfde082ce1b964aeba567e3ecaab58fff794ea4607015ef0709449211a1`];
 
-    let pageNum = 1;
-    let pageSize = 2;
-    let kwd = '토지';
-    let category = '도서';
+let url1 = new URL('https://www.nl.go.kr/NL/search/openApi/search.do?');
+let url = new URL('http://data4library.kr/api/loanItemSrch?');
+let bookList = [];
 
-    async function getBook() {
-        try {
-            url.searchParams.set('key', API_KEY);
-            url.searchParams.set('category', category);
-            url.searchParams.set('kwd', kwd);
-            url.searchParams.set('apiType', 'json');
-            url.searchParams.set('pageSize', pageSize);
-            url.searchParams.set('pageNum', pageNum);
-            const response = await fetch(url);
-            const data = await response.json();
-            bookList = data.result;
-            console.table(bookList);
-            render();
-        } catch (error) {
-            console.log(error);
-        }
+// 화면에 표시되는 줄수
+const lineCount = 3;
+
+// 현재 패이지, 출력 item 수
+let pageNum = 1;
+let pageSize = itemCountCalculator();
+
+// 검색용
+let systemType = '';
+// category는 메인페이지에서 값을 받아서 사용하고 사이드 버튼에서도 사용
+let category = '';
+
+async function getBook() {
+    try {
+        url.searchParams.set('authKey', API_KEY[0]);
+        url.searchParams.set('startDt', '2022-01-01');
+        url.searchParams.set('endDt', '2022-03-31');
+
+        // url.searchParams.set('gender', 1);
+        // url.searchParams.set('age', 20);
+
+        // url.searchParams.set('region', 11);
+        // url.searchParams.set('addCode', 0);
+        // url.searchParams.set('kdc', 6);
+        url.searchParams.set('pageNo', 1);
+        url.searchParams.set('pageSize', 1);
+        url.searchParams.set('format', 'json');
+
+        console.log(url);
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+        bookList = data.response.docs;
+        console.table(bookList);
+
+        moreRender();
+    } catch (error) {
+        console.log(error);
     }
+}
 
-    function render() {
-        let booksHTML = bookList
-            .map(
-                (book) =>
-                    `<div class="card flex-grow-1 m-3">
-            <img src="https://cover.nl.go.kr/${book.imageUrl}" class="card-img-top" alt="..." />
-            <div class="card-body">
-                <h5 class="card-title">${book.titleInfo}</h5>
-            </div>
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">분류:${book.kdcName1s}</li>
-                <li class="list-group-item">매체구분:${book.menuName}</li>
-                <li class="list-group-item">자료유형:${book.typeName}</li>
-                <li class="list-group-item">자료있는곳:${book.placeInfo}</li>
-                <li class="list-group-item">발행자:${book.pubInfo}</li>
-                <li class="list-group-item">발행년도:${book.pubYearInfo}</li>
-            </ul>
-            
-            <div class="card-body">
-                <a href="https://cover.nl.go.kr${book.detailLink}" class="card-link">상세페이지</a>
-            </div>
-        </div>`
-            )
-            .join('');
+function moreRender() {
+    let booksHTML = bookList
+        .map(
+            (book) =>
+                `<div class="card">
+                    <img src="${book.doc.bookImageURL}" alt="" />
+                    <ul>
+                        <li>제목<span>${book.doc.bookname.split(':')[0]}</span></li>
+                        <li>작가<span>${
+                            book.doc.authors.includes(':')
+                                ? book.doc.authors.split(':')[1].trim()
+                                : book.doc.authors.trim()
+                        }</span></li>
+                        <li>분류<span>${book.doc.class_nm.split('>').pop()}</span></li>
+                        <li>발행자<span>${book.doc.publisher}</span></li>
+                        <li>발행일<span>${book.doc.publication_year}</span></li>
+                    </ul>
+                </div>`
+        )
+        .join('');
 
-        console.log(booksHTML);
-        document.getElementById('card-holder').innerHTML = booksHTML;
-    }
+    document.getElementById('mg-card-holder').innerHTML = booksHTML;
+}
 
-    getBook();
-};
+getBook();
+
+function itemCountCalculator() {
+    if (window.innerWidth < 451) return 10;
+    else if (window.innerWidth < 768) return 10;
+    else if (window.innerWidth < 992) return 3 * lineCount;
+    else if (window.innerWidth < 1200) return 4 * lineCount;
+    else if (window.innerWidth < 1400) return 5 * lineCount;
+    else if (window.innerWidth >= 1400) return 6 * lineCount;
+}
+
+window.addEventListener('resize', itemCountCalculator);
+// };
