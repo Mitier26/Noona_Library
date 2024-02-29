e92f59651c3dd80f7bbae562b3ed88bd5c28dd81
YERIELBEIN YOON <younyb33@naver.com>
2024-02-29 22:09:31 +0900

    third commit

--- javascript/script.js
+++ javascript/script.js
@@ -6,87 +6,105 @@
     */
 // API 키를 배열로 만들어 키가 막혔을 때 다른 키를 사용한다.
 // ttbmitier1409002
-const API_KEY = [`09f34dfde082ce1b964aeba567e3ecaab58fff794ea4607015ef0709449211a1`];
-
+const API_KEY = `76920f087955f921eb6b6f79d89fc42703ef032dc51e44b6ee7e4be168f2de59`;
+let url = new URL(`http://data4library.kr/api/loanItemSrch?authKey=${API_KEY}`);
 let url1 = new URL('https://www.nl.go.kr/NL/search/openApi/search.do?');
-let url = new URL('http://data4library.kr/api/loanItemSrch?');
 let bookList = [];
+let popularLoanBooks = [];
 
-// 화면에 표시되는 줄수
-const lineCount = 3;
 
-// 현재 패이지, 출력 item 수
-let pageNum = 1;
-let pageSize = itemCountCalculator();
+// // 화면에 표시되는 줄수
+// const lineCount = 3;
 
-// 검색용
-let systemType = '';
-// category는 메인페이지에서 값을 받아서 사용하고 사이드 버튼에서도 사용
-let category = '';
+// // 현재 패이지, 출력 item 수
+// // let pageNum = 1;
+// // let pageSize = itemCountCalculator();
 
-async function getBook() {
-    try {
-        url.searchParams.set('authKey', API_KEY[0]);
-        url.searchParams.set('startDt', '2022-01-01');
-        url.searchParams.set('endDt', '2022-03-31');
+// // 검색용
+// let systemType = '';
+// // category는 메인페이지에서 값을 받아서 사용하고 사이드 버튼에서도 사용
+// let category = '';
 
-        // url.searchParams.set('gender', 1);
-        // url.searchParams.set('age', 20);
+// console.log('API_KEY : ', API_KEY)
 
-        // url.searchParams.set('region', 11);
-        // url.searchParams.set('addCode', 0);
-        // url.searchParams.set('kdc', 6);
-        url.searchParams.set('pageNo', 1);
-        url.searchParams.set('pageSize', 1);
-        url.searchParams.set('format', 'json');
+// async function getBook() {
+//     try {
+//         url.searchParams.set('authKey', API_KEY);
+//         url.searchParams.set('startDt', '2022-01-01');
+//         url.searchParams.set('endDt', '2022-03-31');
 
-        console.log(url);
-        const response = await fetch(url);
-        const data = await response.json();
-        console.log(data);
-        bookList = data.response.docs;
-        console.table(bookList);
+//         // url.searchParams.set('gender', 1);
+//         // url.searchParams.set('age', 20);
 
-        moreRender();
-    } catch (error) {
-        console.log(error);
-    }
-}
+//         // url.searchParams.set('region', 11);
+//         // url.searchParams.set('addCode', 0);
+//         // url.searchParams.set('kdc', 6);
+//         url.searchParams.set('pageNo', );
+//         url.searchParams.set('pageSize', );
+//         url.searchParams.set('format', 'json');
 
-function moreRender() {
-    let booksHTML = bookList
-        .map(
-            (book) =>
-                `<div class="card">
-                    <img src="${book.doc.bookImageURL}" alt="" />
-                    <ul>
-                        <li>제목<span>${book.doc.bookname.split(':')[0]}</span></li>
-                        <li>작가<span>${
-                            book.doc.authors.includes(':')
-                                ? book.doc.authors.split(':')[1].trim()
-                                : book.doc.authors.trim()
-                        }</span></li>
-                        <li>분류<span>${book.doc.class_nm.split('>').pop()}</span></li>
-                        <li>발행자<span>${book.doc.publisher}</span></li>
-                        <li>발행일<span>${book.doc.publication_year}</span></li>
-                    </ul>
-                </div>`
-        )
-        .join('');
+//         console.log(url);
+//         const response = await fetch(url);
+//         const data = await response.json();
+//         console.log(data);
+//         bookList = data.response.docs;
+//         console.table(bookList);
 
-    document.getElementById('mg-card-holder').innerHTML = booksHTML;
-}
+//         moreRender();
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
 
-getBook();
+// function moreRender() {
+//     let booksHTML = bookList
+//         .map(
+//             (book) =>
+//                 `<div class="card">
+//                     <img src="${book.doc.bookImageURL}" alt="" />
+//                     <ul>
+//                         <li>제목<span>${book.doc.bookname.split(':')[0]}</span></li>
+//                         <li>작가<span>${
+//                             book.doc.authors.includes(':')
+//                                 ? book.doc.authors.split(':')[1].trim()
+//                                 : book.doc.authors.trim()
+//                         }</span></li>
+//                         <li>분류<span>${book.doc.class_nm.split('>').pop()}</span></li>
+//                         <li>발행자<span>${book.doc.publisher}</span></li>
+//                         <li>발행일<span>${book.doc.publication_year}</span></li>
+//                     </ul>
+//                 </div>`
+//         )
+//         .join('');
 
-function itemCountCalculator() {
-    if (window.innerWidth < 451) return 10;
-    else if (window.innerWidth < 768) return 10;
-    else if (window.innerWidth < 992) return 3 * lineCount;
-    else if (window.innerWidth < 1200) return 4 * lineCount;
-    else if (window.innerWidth < 1400) return 5 * lineCount;
-    else if (window.innerWidth >= 1400) return 6 * lineCount;
-}
+//     document.getElementById('mg-card-holder').innerHTML = booksHTML;
+// }
 
-window.addEventListener('resize', itemCountCalculator);
-// };
+// getBook();
+
+// function itemCountCalculator() {
+//     if (window.innerWidth < 451) return 10;
+//     else if (window.innerWidth < 768) return 10;
+//     else if (window.innerWidth < 992) return 3 * lineCount;
+//     else if (window.innerWidth < 1200) return 4 * lineCount;
+//     else if (window.innerWidth < 1400) return 5 * lineCount;
+//     else if (window.innerWidth >= 1400) return 6 * lineCount;
+// }
+
+// window.addEventListener('resize', itemCountCalculator);
+
+
+// 인기도서대출 리스트 뽑기
+
+const getPopularLoanBooks = async () => {
+    url.searchParams.set('format', 'json');
+    const response = await fetch(url);
+    console.log("response : ", response);
+    const data = await response.json();
+    popularLoanBooks = data.response.docs
+    console.log("data : ", popularLoanBooks);
+
+
+};
+
+getPopularLoanBooks();
