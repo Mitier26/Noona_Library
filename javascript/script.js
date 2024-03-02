09752dc13f665c9e5cffbb2298d766c8763006a4
YERIELBEIN YOON <younyb33@naver.com>
2024-03-02 15:51:42 +0900

    eighth commit

--- javascript/script.js
+++ javascript/script.js
@@ -32,91 +32,6 @@ ageMenu.forEach(age =>
 genderMenu.forEach(gender => 
     gender.addEventListener("change",(e) => getPopularLoanBooksByGender(e)));
 
-// // 화면에 표시되는 줄수
-// const lineCount = 3;
-
-// // 현재 패이지, 출력 item 수
-// // let pageNum = 1;
-// // let pageSize = itemCountCalculator();
-
-// // 검색용
-// let systemType = '';
-// // category는 메인페이지에서 값을 받아서 사용하고 사이드 버튼에서도 사용
-// let category = '';
-
-// console.log('API_KEY : ', API_KEY)
-
-// async function getBook() {
-//     try {
-//         url.searchParams.set('authKey', API_KEY);
-//         url.searchParams.set('startDt', '2022-01-01');
-//         url.searchParams.set('endDt', '2022-03-31');
-
-//         // url.searchParams.set('gender', 1);
-//         // url.searchParams.set('age', 20);
-
-//         // url.searchParams.set('region', 11);
-//         // url.searchParams.set('addCode', 0);
-//         // url.searchParams.set('kdc', 6);
-//         url.searchParams.set('pageNo', );
-//         url.searchParams.set('pageSize', );
-//         url.searchParams.set('format', 'json');
-
-//         console.log(url);
-//         const response = await fetch(url);
-//         const data = await response.json();
-//         console.log(data);
-//         bookList = data.response.docs;
-//         console.table(bookList);
-
-//         moreRender();
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
-
-// function moreRender() {
-//     let booksHTML = bookList
-//         .map(
-//             (book) =>
-//                 `<div class="card">
-//                     <img src="${book.doc.bookImageURL}" alt="" />
-//                     <ul>
-//                         <li>제목<span>${book.doc.bookname.split(':')[0]}</span></li>
-//                         <li>작가<span>${
-//                             book.doc.authors.includes(':')
-//                                 ? book.doc.authors.split(':')[1].trim()
-//                                 : book.doc.authors.trim()
-//                         }</span></li>
-//                         <li>분류<span>${book.doc.class_nm.split('>').pop()}</span></li>
-//                         <li>발행자<span>${book.doc.publisher}</span></li>
-//                         <li>발행일<span>${book.doc.publication_year}</span></li>
-//                     </ul>
-//                 </div>`
-//         )
-//         .join('');
-
-//     document.getElementById('mg-card-holder').innerHTML = booksHTML;
-// }
-
-// getBook();
-
-// function itemCountCalculator() {
-//     if (window.innerWidth < 451) return 10;
-//     else if (window.innerWidth < 768) return 10;
-//     else if (window.innerWidth < 992) return 3 * lineCount;
-//     else if (window.innerWidth < 1200) return 4 * lineCount;
-//     else if (window.innerWidth < 1400) return 5 * lineCount;
-//     else if (window.innerWidth >= 1400) return 6 * lineCount;
-// }
-
-// window.addEventListener('resize', itemCountCalculator);
-
-
-// 인기도서대출 리스트 뽑기
-
-
-
 const popularLoanBooksFilter = (e) => {
     let mode = e.target.id
     console.log("mode", mode);
@@ -287,22 +202,4 @@ const moveToPage = (pageNum) => {
     page = pageNum;
     getPopularLoanBooksData();
 }
-
-//  <li class="page-item">
-//     <a class="page-link" href="#" aria-label="Previous">
-//       <span aria-hidden="true">&laquo;</span>
-//     </a>
-//   </li>
-  
-//   <li class="page-item"><a class="page-link" href="#">2</a></li>
-//   <li class="page-item"><a class="page-link" href="#">3</a></li>
-//   <li class="page-item">
-//     <a class="page-link" href="#" aria-label="Next">
-//       <span aria-hidden="true">&raquo;</span>
-//     </a>
-//   </li> 
-
-// 1. 버튼들에 클릭이벤트
-// 2. 지역 별 책들 가져오기
-// 3. 그 책들을 보여주기
 
